36f827a33eb1aa04b958f32d6663e05096d1912d
Imon <imon@flarie.com>
2022-11-02 08:09:16 +0600

    Two Sum II - Input Array Is Sorted

--- two-pointer/167.Two_Sum_II_Input_Array_Is_Sorted.js
+++ two-pointer/167.Two_Sum_II_Input_Array_Is_Sorted.js
@@ -3,9 +3,9 @@ var twoSum = function(numbers, target) {
   let [left, right] = [0, numbers.length-1];
   while(left<right){
     let sum = numbers[left] + numbers[right];
-    if(sum === target) return [left+1, right+1];
-    if(sum>target) right -= 1;
     if(sum<target) left += 1;
+    else if(sum>target) right -= 1;
+    else return [left+1, right+1];
   }    
 };
 
